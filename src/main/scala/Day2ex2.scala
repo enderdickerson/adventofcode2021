46b1dcc500a34778897678dc764c9f79603fbe28
Andrew Dickerson <olah89@gmail.com>
2021-12-02 10:33:32 -0700

    refactor(day2ex2)

--- src/main/scala/Day2ex2.scala
+++ src/main/scala/Day2ex2.scala
@@ -1,12 +1,10 @@
 import scala.io.Source
 
 object Day2ex2 extends App {
-  val aimed = (distance: String, current: Int, aim: Int) => current + (distance.toInt * aim)
-
   def navigate(chart: Seq[String]): Int = {
-    val (hor, depth, aim) = chart.map(s => s.split(" +").toSeq).foldLeft((0,0,0))((acc, cur) => {
+    val (hor, depth, _) = chart.map(s => s.split(" +").toSeq).foldLeft((0,0,0))((acc, cur) => {
       cur match {
-        case Seq("forward", n) => (acc._1 + n.toInt, aimed(n, acc._2, acc._3), acc._3)
+        case Seq("forward", n) => (acc._1 + n.toInt, acc._2 + (n.toInt * acc._3), acc._3)
         case Seq("down", n) => (acc._1, acc._2, acc._3 + n.toInt)
         case Seq("up", n) => (acc._1, acc._2, acc._3 - n.toInt)
         case _ => (acc._1, acc._2, acc._3)
