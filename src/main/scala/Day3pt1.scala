9b9cfcfb5f5d607483451f9a9f0e7ea85b344e63
Andrew Dickerson <olah89@gmail.com>
2021-12-03 09:02:47 -0700

    feat(day3): optimized

--- src/main/scala/Day3pt1.scala
+++ src/main/scala/Day3pt1.scala
@@ -1,22 +1,13 @@
 import scala.io.Source
 
 object Day3pt1 extends App {
-  val findPwrConsumption: Function1[Seq[String], Int] = (input: Seq[String]) => {
-    val asGroups = input.map(_.toList.map(x => x.toString.toInt))
-    val transposed = asGroups.transpose
-    println(asGroups)
-    println(transposed)
-    val withCount = transposed.map(x => x.groupBy(y => y).view.mapValues(z => z.size).toMap)
-    println(withCount)
-    val gamma = withCount.map(x => x.maxBy(_._2)._1)
-    val epsilon = withCount.map(x => x.minBy(_._2)._1)
-
-    val gammaVal = Integer.parseInt(gamma.mkString, 2)
-    val epsilonVal = Integer.parseInt(epsilon.mkString, 2)
-    println(gammaVal)
-    println(epsilonVal)
-    gammaVal * epsilonVal
-  }
+  val findPwrConsumption = (input: Seq[String]) =>
+    input.map(_.toList.map(_.toString.toInt)).transpose
+      .map(_.groupBy(y => y).view.mapValues(_.size).toMap)
+      .map(x => List(x.maxBy(_._2)._1, x.minBy(_._2)._1))
+      .transpose
+      .map(x => Integer.parseInt(x.mkString, 2))
+      .product
 
   println(findPwrConsumption(Source.fromFile("src/main/scala/day3inputtest.txt").getLines.toSeq))
 }
