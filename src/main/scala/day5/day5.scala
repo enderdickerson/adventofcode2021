aa27479561ed82e76bda11a43f4794c40a59aafc
Andrew Dickerson <olah89@gmail.com>
2021-12-05 15:46:02 -0700

    feat(day5): adds part 1 solution

--- src/main/scala/day5/day5.scala
+++ src/main/scala/day5/day5.scala
@@ -7,20 +7,32 @@ object day5 extends App {
   case class Line(start: Coord, end: Coord) {
     def vert(): Boolean = start.x == end.x
     def hor(): Boolean = start.y == end.y
+    def expand(): List[Coord] = {
+      if (vert()) {
+        val range = if (start.y < end.y) start.y to end.y else end.y to start.y
+        range.foldLeft(List[Coord]())((acc, curr) => {
+          acc :+ Coord(start.x, curr)
+        })
+      }
+      else {
+        val range = if (start.x < end.x) start.x to end.x else end.x to start.x
+        range.foldLeft(List[Coord]())((acc, curr) => {
+          acc :+ Coord(curr, start.y)
+        })
+      }
+    }
   }
 
-  val parseLines = (input: Seq[String]) => {
-    val cmdParser = "([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)".r
-    input.map(x => {
-      x match {
-        case cmdParser(x1, y1, x2, y2) => Line(Coord(x1.toInt, y1.toInt), Coord(x2.toInt, y2.toInt))
-      }
-    }).filter(x => x.vert() || x.hor())
-  }
+  val cmdParser = "([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)".r
 
   val findBigVents = (input: Seq[String]) => {
-    parseLines(input)
+    input.map(x => {
+        x match {
+          case cmdParser(x1, y1, x2, y2) => Line(Coord(x1.toInt, y1.toInt), Coord(x2.toInt, y2.toInt))
+        }
+      }).filter(x => x.vert() || x.hor()).flatMap(x => x.expand()).groupBy(x => x).mapValues(_.size)
+      .toMap.filter(x => x._2 > 1).size
   }
 
-  println(findBigVents(Source.fromFile("src/main/scala/day5/day5test.txt").getLines.toSeq))
+  println(findBigVents(Source.fromFile("src/main/scala/day5/day5.txt").getLines.toSeq))
 }
