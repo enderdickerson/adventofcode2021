b7aaf926a451b5ac5b3a2f5e97e6ce660a895672
Andrew Dickerson <olah89@gmail.com>
2021-12-05 09:02:44 -0700

    feat(day5): initial

---
+++ src/main/scala/day5/day5.scala
@@ -0,0 +1,9 @@
+package day5
+
+import scala.io.Source
+
+object day5 extends App {
+  val findBigVents = (input: Seq[String]) => ???
+
+  println(findBigVents(Source.fromFile("src/main/scala/day5/day5test.txt").getLines.toSeq))
+}
