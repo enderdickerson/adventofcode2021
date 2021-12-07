a7e7334530ddc5f24cb7fb2bf18603ecb23913b7
Andrew Dickerson <olah89@gmail.com>
2021-12-06 22:13:11 -0700

    feat(day6): part 2 solution

--- src/main/scala/day6/day6.scala
+++ src/main/scala/day6/day6.scala
@@ -3,11 +3,17 @@ package day6
 import scala.io.Source
 
 object day6 extends App {
-  val growLanternfish = (input: List[String], days: Int) => {
+  val growLanternFish = (input: String, days: Int) => {
     List.range(0, days)
-      .foldLeft(input(0).split(",").map(_.toInt))((acc, _) =>
-        acc.flatMap(fish => if (fish == 0) List(6, 8) else List(fish - 1))).size
+      .foldLeft(input.split(",").map(_.toInt).groupBy(x => x).view.mapValues(x => BigInt(x.size)).toList)((acc, curr) => {
+        acc.flatMap((x, y) => {
+          x match {
+            case 0 => List((8, y), (6, y))
+            case n => List((n - 1, y))
+          }
+        }).groupBy(_._1).map(x => (x._1, x._2.map(_._2).sum)).toList
+      }).map(_._2).sum
   }
 
-  println(growLanternfish(Source.fromFile("src/main/scala/day6/day6.txt").getLines.toList, 80))
+  println(growLanternFish(Source.fromFile("src/main/scala/day6/day6.txt").getLines.toList(0), 256))
 }
