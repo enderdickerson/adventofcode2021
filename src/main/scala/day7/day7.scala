f0b1448ee5e22aa9d8f0b8bb7c74324b2b4a177b
Andrew Dickerson <olah89@gmail.com>
2021-12-06 22:50:25 -0700

    feat(day7): part 2 solution

--- src/main/scala/day7/day7.scala
+++ src/main/scala/day7/day7.scala
@@ -3,7 +3,7 @@ package day7
 import scala.io.Source
 
 object day7 extends App {
-  val calculateConvergence = (input: List[String]) => {
+  val moveCrabsConstant = (input: List[String]) => {
     val crabs = input.map(_.toInt).sorted
     (crabs(0) to crabs.last).foldLeft(Map[Int,Int]())((acc, curr) => {
       acc.updated(curr, crabs.foldLeft(0)((fuel, cb) => {
@@ -12,5 +12,15 @@ object day7 extends App {
     }).minBy(_._2)
   }
 
-  println(calculateConvergence(Source.fromFile("src/main/scala/day7/day7.txt").getLines.toList(0).split(",").toList))
+  val moveCrabsExpensive = (input: List[String]) => {
+    val crabs = input.map(_.toInt).sorted
+    (crabs(0) to crabs.last).foldLeft(Map[Int,Int]())((acc, curr) => {
+      acc.updated(curr, crabs.foldLeft(0)((fuel, cb) => {
+        fuel + (1 to (cb - curr).abs).foldLeft(0)(_ + _)
+      }))
+    }).minBy(_._2)
+  }
+
+  println(moveCrabsConstant(Source.fromFile("src/main/scala/day7/day7.txt").getLines.toList(0).split(",").toList))
+  println(moveCrabsExpensive(Source.fromFile("src/main/scala/day7/day7.txt").getLines.toList(0).split(",").toList))
 }
