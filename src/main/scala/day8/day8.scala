383b7d79089752ea1c6432404dacc0d9f4adc010
Andrew Dickerson <olah89@gmail.com>
2021-12-08 10:39:43 -0700

    feat(day8): initial

---
+++ src/main/scala/day8/day8.scala
@@ -0,0 +1,9 @@
+package day8
+
+import scala.io.Source
+
+object day8 extends App {
+  val calculate = (input: List[String]) => ???
+
+  println(calculate(Source.fromFile("src/main/scala/day8/day8test.txt").getLines.toList))
+}
