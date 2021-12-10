1308343c0a7a3e11eb2f7485673403847edc074b
Andrew Dickerson <olah89@gmail.com>
2021-12-10 10:55:48 -0700

    feat(day10): part 2

--- src/main/scala/day10/day10.scala
+++ src/main/scala/day10/day10.scala
@@ -3,36 +3,26 @@ package day10
 import scala.io.Source
 
 object day10 extends App {
-  val matchesOpener = (opener: Char, closer: Char) => {
-    val expectedCloser = opener match {
+  val getCloser = (opener: Char) => {
+    opener match {
       case '[' => ']'
       case '(' => ')'
       case '{' => '}'
       case '<' => '>'
     }
-    expectedCloser == closer
   }
 
   val checkLine = (input: String) => {
     input.toList.foldLeft((List[Char](), ""))((acc, curr) => {
       if (!acc._2.isEmpty) acc
-      else if ("{<([".toList.contains(curr)) {
-//        println(s"found opening: $curr")
-        (acc._1 :+ curr, "")
-      }
-      else if (!matchesOpener(acc._1.last, curr)) {
-//        println(s"found error $curr - expected ${acc._1.last}")
-        (acc._1, curr.toString)
-      }
-      else {
-//        println(s"found matching closer $curr for ${acc._1.last}")
-        (acc._1.dropRight(1), "")
-      }
-    })._2
+      else if ("{<([".toList.contains(curr)) (acc._1 :+ curr, "")
+      else if (getCloser(acc._1.last) != curr) (acc._1, curr.toString)
+      else (acc._1.dropRight(1), "")
+    })
   }
 
   val calcLineScore = (input: List[String]) => {
-    input.map(x => checkLine(x)).filter(x => !x.isEmpty).map(x => {
+    input.map(x => checkLine(x)._2).filter(x => !x.isEmpty).map(x => {
       x match {
         case ")" => 3
         case "]" => 57
@@ -42,5 +32,30 @@ object day10 extends App {
     }).sum
   }
 
-  println(calcLineScore(Source.fromFile("src/main/scala/day10/day10.txt").getLines.toList))
+  val autocomplete = (input: List[Char]) => {
+    input.reverse.foldLeft(List[Char]())((acc, curr) => {
+      acc :+ getCloser(curr)
+    })
+  }
+
+  val score = (input: List[Char]) => {
+    input.map(x => {
+      x match {
+        case ')' => 1
+        case ']' => 2
+        case '}' => 3
+        case '>' => 4
+      }
+    }).foldLeft(BigInt(0))((acc, curr) => {
+      acc * 5 + curr
+    })
+  }
+
+  val calcAutocomplete = (input: List[String]) => {
+    val scores = input.map(x => checkLine(x)).filter((x,y) => y.isEmpty).map((x,y) => autocomplete(x)).map(score).sorted
+    scores((scores.size - 1) / 2)
+  }
+
+//  println(calcLineScore(Source.fromFile("src/main/scala/day10/day10.txt").getLines.toList))
+  println(calcAutocomplete(Source.fromFile("src/main/scala/day10/day10.txt").getLines.toList))
 }
