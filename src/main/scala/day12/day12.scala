cdb0b75d68309d604249044ad9a9286f8fd566e6
Andrew Dickerson <olah89@gmail.com>
2021-12-13 08:47:50 -0700

    feat(day12): init

---
+++ src/main/scala/day12/day12.scala
@@ -0,0 +1,9 @@
+package day12
+
+import scala.io.Source
+
+object day12 extends App {
+  val calc = (input: List[String]) => ???
+
+  println(calc(Source.fromFile("src/main/scala/day12/day12test.txt").getLines.toList))
+}
